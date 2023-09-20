8f8002f7c5b395c0dfef3585532356a60d36dbd8
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-09-20 17:40:23 +0200

    Add project files.

---
+++ Assignment4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void MyFourthMethod(string word)
+    {
+        Console.WriteLine($"Please enter a {word}");
+        string userName = Console.ReadLine();
+        Console.WriteLine(userName);
+    }
+
+    static void Main(string[] args)
+    {
+        MyFourthMethod("girl name");
+    }
+}
